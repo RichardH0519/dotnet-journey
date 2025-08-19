39aa932af9613a82c3abdf1ffb7d11baa6f91537
Quoc Thinh Huynh (Richard) <thinhwtnb@gmail.com>
2025-08-19 19:39:53 +1000

    Added HelloWorld sln and SimpleCalculator sln

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace SimpleCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Get user input
+            Console.WriteLine("Enter first number:");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+            int result = firstNumber + secondNumber;
+
+            Console.WriteLine("Result: " + result);
+
+            Console.ReadKey();
+        }
+    }
+}
