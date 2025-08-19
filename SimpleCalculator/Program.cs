4dd7819a8de49cf94d3caf0aa581df50c4c4ac39
Quoc Thinh Huynh (Richard) <thinhwtnb@gmail.com>
2025-08-19 20:46:56 +1000

    Updated SimpleCalculator

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -9,9 +9,8 @@
             int firstNumber = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Enter second number:");
             int secondNumber = Convert.ToInt32(Console.ReadLine());
-            int result = firstNumber + secondNumber;
 
-            Console.WriteLine("Result: " + result);
+            Console.WriteLine($"Result: {firstNumber + secondNumber}");
 
             Console.ReadKey();
         }
